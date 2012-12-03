f315f6b3d52de2e7e752e5630e6d8040899a97ec
Bart Massey <bart@cs.pdx.edu>
2012-12-03 12:06:46 -0800

    fixed region parser; successfully parsed region

--- parse-region.hs
+++ parse-region.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import qualified Codec.Compression.GZip as GZip
-import qualified Codec.Compression.Zlib.Raw as Deflate
+import qualified Codec.Compression.Zlib as Zlib
 import Control.Monad
 import Data.Bits
 import qualified Data.ByteString as BS
@@ -13,6 +13,7 @@ import Data.Int
 import Data.NBT
 import Data.Serialize
 import Data.Word
+import System.Environment
 
 eitherErr :: Either String a -> a
 eitherErr = either error id
@@ -24,11 +25,9 @@ data ChunkIndex = ChunkIndex {
   ciTimeStamp :: Word32
 } deriving Show
 
-parseRegionFile :: String -> IO [ChunkIndex]
-parseRegionFile fn = do
-  regionFile <- BS.readFile fn
-  let regions = eitherErr $ runGet parseHeader regionFile
-  return regions
+parseRegionHeader :: BS.ByteString -> [ChunkIndex]
+parseRegionHeader regionFile =
+  eitherErr $ runGet parseHeader regionFile
   where
     parseHeader = do
       locBlocks <- replicateM 1024 getWord32be
@@ -44,3 +43,33 @@ parseRegionFile fn = do
             ciFileOffset = 4096 * (block `shiftR` 8),
             ciApproxLength = 4096 * (block .&. 0xff),
             ciTimeStamp = ts }
+
+data ChunkData = ChunkData {
+  cdLength :: Word32,
+  cdCompressionType :: Word8,
+  cdChunk :: NBT } deriving Show
+
+getChunk :: Word32 -> BS.ByteString -> ChunkData
+getChunk offset regionFile =
+  eitherErr $ runGet parseChunk regionFile
+  where
+    parseChunk = do
+      skip (fromIntegral offset)
+      chunkLength <- getWord32be
+      compression <- getWord8
+      unless (compression == 2) 
+        (error $ "unexpected compression format " ++ show compression)
+      compressedChunk <- getBytes (fromIntegral chunkLength)
+      let chunk = Zlib.decompress $ LBS.pack $ BS.unpack compressedChunk
+      let nbt = eitherErr $ decodeLazy chunk
+      return $ ChunkData {
+        cdLength = chunkLength,
+        cdCompressionType = compression,
+        cdChunk = nbt }
+
+main :: IO ()
+main = do
+  [fn] <- getArgs
+  regionFile <- BS.readFile fn
+  let chunkIndexes = parseRegionHeader regionFile
+  print $ getChunk (ciFileOffset $ head chunkIndexes) regionFile
